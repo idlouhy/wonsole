9d3541d25db2fbfcaf0e108f079b744949dc1496
Oddwarg <mogfen@yahoo.com>
2012-10-02 23:09:20 +0200

    All UI functionality is now networked

--- www/wonsole/LibraryObjects.js
+++ www/wonsole/LibraryObjects.js
@@ -3,18 +3,28 @@ function Library() {
     var self    = this;
     this.list   = new Array();
     this.selectAll = false;
-
+    
+    /*Should be set to the number of books to be deleted before calling the private removeBookAtIndex function.*/
+    var removeCount = 0;
+    
     this.deleteSelected = deleteSelected ;
     /**Delete all books that are selected in the visible list, will update the web UI.*/
     function deleteSelected () {
+        $.blockUI();
+        if(removeCount!=0)
+            alert("removeCount must be 0 here!");
         for(c=0; c<self.list.length; c++) {
             if(self.list[c].select) {
-                self.list.splice(c,1);
+                removeCount ++;
+            }
+        }
+        for(c=0; c<self.list.length; c++) {
+            if(self.list[c].select) {
+                removeBookAtIndex(c);
                 c--;
             }
         }
         self.selectAll = false;
-        self.generateHTML();
     }
 
     this.selectAllToggle = selectAllToggle;
@@ -46,7 +56,7 @@ function Library() {
         cell.innerHTML = "ID";
         row.appendChild(cell);
         cell = document.createElement('td');
-        cell.innerHTML = "Name";
+        cell.innerHTML = "Title";
         row.appendChild(cell);
         cell = document.createElement('td');
         cell.innerHTML = "Author";
@@ -76,38 +86,92 @@ function Library() {
         loc.appendChild(tab);
     }
     
-    /**Remove the book at position i in the list*/
-    this.removeBook = removeBook;
-    function removeBook(i) {
-        self.list.splice(i,1);
-        self.generateHTML();
+    /*Private function to remove a book. Does not lock the UI by itself.
+     *removeCount should be set to the total number of books to be deleted in this batch, before this is called.
+     *When this has been called removeCount times and the books have been deleted, the UI is unblocked and the UI is updated.*/
+    var removeBookAtIndex = function(index) {
+        /*Remove book from local storage immediately. If this is ever changed, delete button loop must also be changed!*/
+        var deletedBook = self.list[index];
+        self.list.splice(index,1);
+        $.ajax({
+            type: "DELETE",
+            url: "http://netlight.dlouho.net:9004/api/books/"+deletedBook.id,
+            success: function(response) {
+               removeCount--;
+               /*Check if all books that should be deleted have been deleted now.*/
+               if(removeCount==0) {
+                   self.generateHTML();
+                   $.unblockUI();
+               }
+            },
+            dataType: "text"
+        });
+    }
+
+    this.retrieveObjects = retrieveObjects;
+    /*This function retrieves all objects from the server and updates the web UI. Will lock the UI until objects have been received.*/
+    function retrieveObjects() {
+        $.blockUI();
+        $.getJSON("http://netlight.dlouho.net:9004/api/books" ,function (data) {
+            for(var i = 0; i<data.length; i++)
+                new Book(data[i]._id,data[i].title,data[i].author);
+            generateHTML();
+            $.unblockUI();
+        });
     }
 }
 var LIBRARY = new Library();
+function initLibrary() {
+    LIBRARY.retrieveObjects();
+    }
 
-
-/**Book object containing information about a single book.*/
-function Book(name, author) {
+/**Book object containing information about a single book and add it to the list of Books. 
+  *If id is null, the object will be sent to the server, and the id will be returned. This will block the UI and then update it.
+  *id should be null when using this from the console or web UI.*/
+function Book(id, title, author) {
     var self    = this;
-    this.name   = name;  
+    this.title   = title;  
     this.author = author;
-    this.id     = null;
+    this.id     = id;
     
     this.select = false;
     this.checkbox = null;
-
+    
+    this.saveUpdate = saveUpdate;
+    /*Update the book on the server, blocking/unblocking and updating the UI in the process.*/
+    function saveUpdate() {
+        $.blockUI();
+        $.ajax({
+            type: "PUT",
+            url: "http://netlight.dlouho.net:9004/api/books/"+self.id,
+            data: self.toJSON(),
+            success: function(response) {
+               self.generateHTML();
+               $.unblockUI();
+            },
+            dataType: "json"
+        });
+        LIBRARY.generateHTML();
+    }
+    
     this.changeAuthor = changeAuthor;
     /**Change the author of the book. Will update the web UI.*/
     function changeAuthor(newAuthor) {
         self.author = newAuthor;
-        LIBRARY.generateHTML();
+        self.saveUpdate();
     }
+    
+    this.toJSON = toJSON;
+    /*Generate a JSON object from this Book.*/
+    function toJSON() {
+        return { _id:self.id, title: self.title, author: self.author};
+    }    
 
-    this.changeName = changeName;
+    this.changeTitle = changeTitle;
     /**Change the name of the book. Will update the web UI.*/
-    function changeName(newName) {
-        self.name = newName;
-        LIBRARY.generateHTML();
+    function changeTitle(newTitle) {
+        self.title = newTitle;
+        self.saveUpdate();
     }
 
     this.toggleSelect = toggleSelect;
@@ -120,7 +184,7 @@ function Book(name, author) {
 
     this.generateHTML = generateHTML;
     /**Generate HTML element(s) for this book.
-      *@return The string for the HTML element. Row style will be overridden.*/
+      *@return The string for the HTML element. Should return a table row. Row style will be overridden.*/
     function generateHTML() {
         var row, cell, input;
         row = document.createElement("tr");
@@ -142,9 +206,9 @@ function Book(name, author) {
         cell = document.createElement("td");
         input = document.createElement("input");
         input.type = "text";
-        input.id = "NAME_"+self.id;
-        input.value = self.name;
-        input.onchange = function(){self.changeName(this.value);};
+        input.id = "TITLE_"+self.id;
+        input.value = self.title;
+        input.onchange = function(){self.changeTitle(this.value);};
         cell.appendChild(input );
         row.appendChild(cell);
         
@@ -160,9 +224,26 @@ function Book(name, author) {
         return row;
     }
     
+    /*Add the book to the list*/
     LIBRARY.list.push(this);
-    return LIBRARY.generateHTML();
+    
+    /*Send the book to the server and retrieve id if none was supplied.*/
+    if(id==null) {
+        $.blockUI();
+        $.ajax({
+            type: "POST",
+            url: "http://netlight.dlouho.net:9004/api/books",
+            data: self.toJSON(),
+            success: function(response){
+                //Add book to local storage
+                self.id = response._id;
+                LIBRARY.generateHTML();
+                $.unblockUI();
+            },
+            dataType: "json"
+        });
+    }
 }
 Book.prototype.toString = function(){
-    return "[object Book <"+this.id+", "+this.name+", "+this.author+">]"
+    return "[object Book <"+this.id+", "+this.title+", "+this.author+">]"
 }
