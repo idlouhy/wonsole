39a2810370dc0cc7994e3e0e04d7b1a8c1848fb3
idlouhy <idlouhy@gmail.com>
2012-11-03 18:56:35 +0100

    script to generate screenshot figures into latex

---
+++ manual/user/src/generate.py
@@ -0,0 +1,21 @@
+import os
+
+out = open("screenshot.tex", "w");
+count = 0
+
+out.write("\\centering\n")
+for file in os.listdir("screenshot"):
+  if file.endswith("png"):
+    file = file.replace("_", "\\_")
+    out.write("\\begin{figure}\n")
+    out.write("\\includegraphics[width=\\textwidth]{screenshot/"+file+"}\n")
+    out.write("\\caption{"+file+"}\n")
+    out.write("\\label{"+file+"}\n")
+    out.write("\\end{figure}\n\n\n")
+    count = count +1
+    if count == 2:
+      out.write("\\clearpage\n")
+      count = 0
+    
+
+out.close()
